04ffd1d3de057d71a004f26e786228b932875c89
t79112558208 <t79112558208@ya.ru>
2025-01-08 13:46:40 +0300

    Update supervised-installer.fixed.sh

--- supervised-installer.fixed.sh
+++ supervised-installer.fixed.sh
@@ -261,7 +261,7 @@ EOF
 ##
 # Pull supervisor image
 info "Install supervisor Docker container"
-docker pull "$HASSIO_DOCKER:$HASSIO_VERSION" > /dev/null
+docker pull "$HASSIO_DOCKER:latest" > /dev/null
 docker tag "$HASSIO_DOCKER:$HASSIO_VERSION" "$HASSIO_DOCKER:latest" > /dev/null
 
 ##
